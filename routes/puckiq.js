6e10831ca47ca2cc63d8bb24ec37f7065a4756ee
Sean Solbak <sean@samdesk.io>
2019-02-26 21:06:17 -0700

    add ability to download via csv

--- routes/puckiq.js
+++ routes/puckiq.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const express = require('express');
 const Request = require('request');
+const stringify = require('csv-stringify/lib/es5');
 
 const encode_query = (query) => {
     return _.chain(_.keys(query))
@@ -10,9 +11,11 @@ const encode_query = (query) => {
 
 function PuckIQHandler(app, request, config, cache) {
 
+    let controller = this;
+
     let baseUrl = config.api.host;
 
-    this.getHome = function(req, res) {
+    controller.getHome = function(req, res) {
         app.use(express.static('views/home/public'));
         cache.init().then((iq) => {
 
@@ -31,24 +34,74 @@ function PuckIQHandler(app, request, config, cache) {
                 divisions : divisions,
                 season : iq.current_woodmoney_season
             });
+
         }, (err) => {
             console.log("Error: " + err); //TODO better
             res.render('500');
         });
     };
 
-    this.getAbout = function(req, res) {
+    controller.getAbout = function(req, res) {
         app.use(express.static('views/home/public'));
         res.render('home/about', { pgname: 'about', layout : '__layouts/main' });
     };
 
-    this.getPlayerWowy = function(req, res) {
+    controller.getPlayerWowy = function(req, res) {
         app.use(express.static('views/player-wowy/public'));
         res.render('player-wowy/index', { pgname: 'player-wowy' });
     };
 
-    this.getPlayerWoodmoney = function(req, res) {
+    controller.getPlayerWoodmoney = function(req, res) {
         app.use(express.static('views/player-woodmoney/public'));
+
+        controller._getPlayerWoodmoney(req, res, (err, data) => {
+
+            if(err) {
+                console.log("Error: " + err); //TODO better
+                return res.render('500');
+            }
+
+            let page = _.extend({
+                title: `PuckIQ | ${data.playerName}`,
+                layout: '__layouts/main2'
+            }, data);
+
+            res.render('player-woodmoney/index', page);
+
+        });
+    };
+
+    controller.downloadPlayerWoodmoney = function(req, res) {
+
+        controller._getPlayerWoodmoney(req, res, (err, data) => {
+
+            if(err) {
+                console.log("Error: " + err); //TODO better
+                return res.render('500');
+            }
+
+            let records = _.map(data.playerStats, x => {
+                let row = _.extend({
+                    playerId: data.playerID,
+                    playerName: data.playerName,
+                    position: data.playerPosition
+                }, x);
+                return _.values(row);
+            });
+
+            let player_name = data.playerName.replace(/\s/g, "_");
+            let file_name = `${player_name}_woodmoney.csv`;
+
+            res.setHeader('Content-Disposition', `attachment; filename=${file_name}`);
+            res.setHeader('Content-Type', 'text/csv');
+
+            stringify(records, {quoted_string: true}, (err, content) => {
+                res.send(content);
+            });
+        });
+    };
+
+    controller._getPlayerWoodmoney = function(req, res, done){
 
         let player_id = req.params.player;
 
@@ -56,63 +109,109 @@ function PuckIQHandler(app, request, config, cache) {
 
         Request.get({ url: url, json: true }, (err, response, data) => {
 
-            let content = massagePlayerResponse(player_id, data);
-            let page = _.extend({
-                title: `PuckIQ | ${content.playerName}`,
-                layout: '__layouts/main2'
-            }, content);
+            if (err) {
+                return done(err);
+            } else if (response.statusCode !== 200) {
+                return done("Unhandle response " + response);
+            }
 
-            res.render('player-woodmoney/index', page);
+            return done(null, massagePlayerResponse(player_id, data));
         });
+
     };
 
-    this.getTeamWoodmoney = function(req, res) {
+    controller.getTeamWoodmoney = function(req, res) {
         app.use(express.static('views/team-woodmoney/public'));
+
+        controller._getTeamWoodmoney(req, res, (err, data) => {
+
+            if(err) {
+                console.log("Error: " + err); //TODO better
+                return res.render('500');
+            }
+
+            let page = _.extend({
+                title: `PuckIQ | ${data.team.name} | ${data.season}`,
+                layout: '__layouts/main2'
+            }, data);
+
+            res.render('team-woodmoney/index', page);
+        });
+
+    };
+
+    controller.downloadTeamWoodmoney = function(req, res) {
+
+        controller._getTeamWoodmoney(req, res, (err, data) => {
+
+            if(err) {
+                console.log("Error: " + err); //TODO better
+                return res.render('500');
+            }
+
+            let records = _.map(data.players, x => {
+                let row = _.extend({season: data.seasonId, team: data.team.name}, x);
+                return _.values(row);
+            });
+
+            let team_name = data.team.name.replace(/\s/g, "_");
+            let file_name = `${team_name}_woodmoney.csv`;
+
+            res.setHeader('Content-Disposition', `attachment; filename=${file_name}`);
+            res.setHeader('Content-Type', 'text/csv');
+
+            stringify(records, {quoted_string: true}, (err, content) => {
+                res.send(content);
+            });
+        });
+
+    };
+
+    controller._getTeamWoodmoney = function(req, res, done) {
 
         let team_id = req.params.team;
 
-        if(!team_id) return res.jsonp([]); //todo better
+        if (!team_id) return res.jsonp([]); //todo better
 
         cache.init().then((iq) => {
+
             let current_season = iq.current_woodmoney_season;
             let season_id = req.query.season ? req.query.season : current_season && current_season._id;
 
-            let options = { season : season_id};
+            let options = {season: season_id};
 
             let url = `${baseUrl}/woodmoney/teams/${team_id}?${encode_query(options)}`;
-            console.log(url);
-
             let team = iq.teams[team_id.toLowerCase()];
 
-            Request.get({ url: url, json: true }, (err, response, data) => {
-                let content = massageTeamResponse(team, season_id, data);
-                let page = _.extend({
-                    title: `PuckIQ | ${team.name} | ${content.season}`,
-                    layout: '__layouts/main2'
-                }, content);
-
-                res.render('team-woodmoney/index', page);
-
+            Request.get({url: url, json: true}, (err, response, data) => {
+                if (err) {
+                    return done(err);
+                } else if (response.statusCode !== 200) {
+                    return done("Unhandle response " + response);
+                } else {
+                    return done(null, massageTeamResponse(team, season_id, data));
+                }
             }, (err) => {
                 console.log("Error: " + err); //TODO better
-                res.render('500');
+                return done(err);
             });
-
         });
+
     };
 
-    this.getTemplate = function(req, res) {
+    controller.getTemplate = function(req, res) {
         app.use(express.static('views/_template/public'));
         res.render('_template/index', { pgname: 'template' });
     };
 
-    this.get404 = function(req, res) {
+    controller.get404 = function(req, res) {
         app.use(express.static('views/error404/public'));
         res.render('error404/index');
     };
 }
 
 function massageTeamResponse(team, seasonId, responseJSON) {
+
     var players = [];
     for(var i = 0; i < responseJSON.length; i++) {
         players.push(massagePlayerData(responseJSON[i]));
@@ -127,8 +226,8 @@ function massageTeamResponse(team, seasonId, responseJSON) {
 }
 
 function massagePlayerResponse(playerID, responseJSON) {
-    let player = extractPlayerInfo(responseJSON[0])
-    let stats = []
+    let player = extractPlayerInfo(responseJSON[0]);
+    let stats = [];
     for(var i = 0; i < responseJSON.length; i++) {
         stats.push(massagePlayerData(responseJSON[i]));
     }
